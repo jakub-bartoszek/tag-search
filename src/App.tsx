1fd31ffa937da9237d77b57671bea4f8c59ffe2e
Jakub Bartoszek <k.bartoszek507@wp.pl>
2024-03-28 12:34:27 +0100

    added conditional rendering

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,28 @@
+import { useEffect } from "react";
+import { fetchTagsStart, selectError, selectLoading, selectTags } from "./utils/redux/tagsSlice";
+import { useDispatch, useSelector } from "react-redux";
+
 function App() {
- return <></>;
+ const dispatch = useDispatch();
+ const tags = useSelector(selectTags);
+ const loading = useSelector(selectLoading);
+ const error = useSelector(selectError);
+
+ useEffect(() => {
+  dispatch(fetchTagsStart());
+ }, []);
+
+ if (error) {
+  return <>{error}</>;
+ }
+
+ if (loading) {
+  return <>Loading</>;
+ }
+
+ if (tags) {
+  return <>Tags</>;
+ }
 }
 
 export default App;
