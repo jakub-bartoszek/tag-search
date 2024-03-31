b84e7c38af53b2b38ad34f0500ca6a36594e6e2b
Jakub Bartoszek <k.bartoszek507@wp.pl>
2024-03-31 23:10:34 +0200

    taking search params from url

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,24 @@
 import { useEffect } from "react";
 import { fetchTagsStart, selectError, selectLoading, selectTags } from "./utils/redux/tagsSlice";
 import { useDispatch, useSelector } from "react-redux";
+import { useLocation, useSearchParams } from "react-router-dom";
 
 function App() {
  const dispatch = useDispatch();
  const tags = useSelector(selectTags);
  const loading = useSelector(selectLoading);
  const error = useSelector(selectError);
+ const [searchParams, setSearchParams] = useSearchParams();
+ const location = useLocation();
 
  useEffect(() => {
-  dispatch(fetchTagsStart());
- }, []);
+  dispatch(fetchTagsStart(location.search));
+ }, [location.search, dispatch]);
+
+ const handlePageSizeChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
+  searchParams.set("pagesize", e.target.value);
+  setSearchParams(searchParams);
+ };
 
  if (error) {
   return <>{error}</>;
@@ -21,7 +29,30 @@ function App() {
  }
 
  if (tags) {
-  return <>Tags</>;
+  return (
+   <>
+    <div>
+     <label htmlFor="pageSize">Page size:</label>
+     <select
+      id="pageSize"
+      value={searchParams.get("pagesize") || "20"}
+      onChange={handlePageSizeChange}
+     >
+      {[...Array(20)].map((_, index) => (
+       <option
+        key={index + 1}
+        value={index + 1}
+       >
+        {index + 1}
+       </option>
+      ))}
+     </select>
+    </div>
+    {tags.map((tag) => (
+     <p key={tag.id}>{tag.name}</p>
+    ))}
+   </>
+  );
  }
 }
 
