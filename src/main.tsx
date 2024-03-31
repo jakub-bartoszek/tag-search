afc04a75df13d063d837c46279410c8583ba8eb1
Jakub Bartoszek <k.bartoszek507@wp.pl>
2024-03-31 23:10:44 +0200

    wrapped with hash router

--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,14 @@ import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import { Provider } from "react-redux";
 import store from "./utils/redux/store.tsx";
+import { HashRouter } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
  <React.StrictMode>
   <Provider store={store}>
-   <App />
+   <HashRouter>
+    <App />
+   </HashRouter>
   </Provider>
  </React.StrictMode>
 );
