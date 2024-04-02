0eea0491f668dcb45e30f32fd3299c774cfb6553
Jakub Bartoszek <k.bartoszek507@wp.pl>
2024-04-03 00:13:19 +0200

    created TagList component

---
+++ src/components/TagList.tsx
@@ -0,0 +1,29 @@
+import { List, ListItem, Typography } from "@mui/material";
+import { Tag } from "../App";
+
+interface TagListProps {
+ tags: Tag[];
+}
+
+const TagList = ({ tags }: TagListProps) => {
+ return (
+  <List>
+   {tags.map((tag, index) => (
+    <ListItem
+     key={tag.id}
+     sx={{
+      display: "flex",
+      justifyContent: "space-between",
+      paddingX: 0,
+      borderBottom: index !== tags.length - 1 ? "1px solid #ccc" : "none"
+     }}
+    >
+     <Typography>{tag.name}</Typography>
+     <Typography>{tag.count}</Typography>
+    </ListItem>
+   ))}
+  </List>
+ );
+};
+
+export default TagList;
