54009fdbd1725cfad2d921b3bace7c7a16a3d2e3
Jakub Bartoszek <k.bartoszek507@wp.pl>
2024-04-02 18:24:01 +0200

    imported main,.css

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,7 @@ import App from "./App.tsx";
 import { Provider } from "react-redux";
 import store from "./utils/redux/store.tsx";
 import { HashRouter } from "react-router-dom";
+import "./main.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
  <React.StrictMode>
