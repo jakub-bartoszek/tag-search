18146bfe4a3dd660acfb870f40a2d16338c244ad
Jakub Bartoszek <k.bartoszek507@wp.pl>
2024-04-03 00:12:57 +0200

    created Pagination component

---
+++ src/components/Pagination.tsx
@@ -0,0 +1,35 @@
+import { Box, Button, Typography } from "@mui/material";
+
+interface PaginationProps {
+ page: number;
+ handlePageChange: (newPage: number) => void;
+}
+
+const Pagination = ({ page, handlePageChange }: PaginationProps) => {
+ return (
+  <Box
+   display={"flex"}
+   alignItems={"center"}
+   justifyContent={"center"}
+   marginTop={2}
+   gap={2}
+  >
+   <Button
+    disabled={page === 1}
+    onClick={() => handlePageChange(page - 1)}
+    variant="outlined"
+   >
+    &#11164;
+   </Button>
+   <Typography variant="subtitle1">{page}</Typography>
+   <Button
+    onClick={() => handlePageChange(page + 1)}
+    variant="outlined"
+   >
+    &#11166;
+   </Button>
+  </Box>
+ );
+};
+
+export default Pagination;
